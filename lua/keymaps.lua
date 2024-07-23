e4a3676822ee886bb9584ba88fe469663d7b5b41
Zz1 <1626741109lzz@gmail.com>
2024-07-23 20:49:04 +0800

    keybinding changed(-)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,15 +5,15 @@ keyset('i', "<C-f>", "<right>", opts)
 keyset('i', "<C-b>", "<left>", opts)
 keyset('i', "<C-n>", "<down>", opts)
 keyset('i', "<C-p>", "<up>", opts)
-keyset('i', "<M-b>", "<C-left>", opts)
-keyset('i', "<M-f>", "<C-right>", opts)
 
-keyset('i', "<C-l>", "<Esc>zza", opts)
+keyset('i', "<M-f>", "<C-o>e<C-o>l", opts)
+keyset('i', "<M-b>", "<C-o>b", opts)
+
+keyset('i', "<C-l>", "<C-o>zz", opts)
 keyset('i', "<C-e>", "<End>", opts)
 keyset('i', "<C-a>", "<Home>", opts)
-keyset('i', "<C-y>", "<Esc>pa", opts)
+keyset('i', "<C-y>", "<C-o>p", opts)
 keyset("n", "<leader>w", "<CMD>w<CR>", opts)
-
 keyset('i', "<C-space>", " ", opts) -- for gui, avoid touching by mistake
 
 keyset({ 'n', 'o', 'x' }, "<C-a>", "^", opts)
