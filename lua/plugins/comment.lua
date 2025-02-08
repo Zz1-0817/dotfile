3b35ea8305fc76e86ad5edd39264aa3f52f85119
Zz1iu <1626741109lzz@gmail.com>
2025-02-08 16:26:03 +0800

    feat.(comment) use built-in code comment now

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,11 +1,5 @@
 return {
     {
-        'echasnovski/mini.comment',
-        event = { "bufreadpost", "bufwritepost", "bufnewfile" },
-        version = false,
-        opts = {}
-    },
-    {
         "folke/todo-comments.nvim",
         event = { "bufreadpost", "bufwritepost", "bufnewfile" },
         dependencies = { "nvim-lua/plenary.nvim" },
