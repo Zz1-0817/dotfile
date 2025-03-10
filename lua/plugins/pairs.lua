1501e6bb4fb979c8784bb9977f518605c6761b40
Zz1iu <1626741109lzz@gmail.com>
2025-03-10 15:35:07 +0800

    fix(snippet). removing `nvim-ts-autotag` support for markdown, which is contradict with my snippet configuration.

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -89,11 +89,19 @@ return {
         "windwp/nvim-ts-autotag",
         ft = {
             "astro", "glimmer", "handlebars", "html",
-            "javascript", "jsx", "markdown", "php",
+            "javascript", "jsx", "php",
             "rescript", "svelte", "tsx", "twig",
             "typescript", "vue", "xml",
         },
-        opts = {}
+        opts = {
+            per_filetype = {
+                ['markdown'] = {
+                    enable_close = false,
+                    enable_rename = false,
+                    enable_close_on_slash = false
+                }
+            }
+        }
     },
     {
         "kylechui/nvim-surround",
